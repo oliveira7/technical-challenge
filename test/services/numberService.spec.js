c6cb404a4f9c2693c97d426b5c411fb821a670cb
Yuri Oliveira <deoliveirayuri7@yahoo.com>
2021-12-03 19:40:47 -0300

    fix/test-and-prime-function

--- test/services/numberService.spec.js
+++ test/services/numberService.spec.js
@@ -1,12 +1,20 @@
 const NumberService = require("../../src/services/NumberService");
 const NumberServiceInstance = new NumberService();
 
-const dividerResponse = [1, 3, 5, 9, 15, 45];
-const primeResponse = [3, 5];
+const dividerResponse = [
+  1, 3, 5, 7, 13, 15, 19, 21, 35, 37, 39, 57, 65, 91, 95, 105, 111, 133, 185,
+  195, 247, 259, 273, 285, 399, 455, 481, 555, 665, 703, 741, 777, 1235, 1295,
+  1365, 1443, 1729, 1995, 2109, 2405, 3367, 3515, 3705, 3885, 4921, 5187, 7215,
+  8645, 9139, 10101, 10545, 14763, 16835, 24605, 25935, 27417, 45695, 50505,
+  63973, 73815, 137085, 191919, 319865, 959595,
+];
+const primeResponse = [
+  3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+];
 
 describe("numberService", () => {
   it("Success in catching all dividers", () => {
-    const response = NumberServiceInstance.divider(45);
+    const response = NumberServiceInstance.divider(959595);
 
     expect(response).toEqual(dividerResponse);
   });
@@ -16,7 +24,7 @@ describe("numberService", () => {
 
     expect(response).toEqual([1]);
   });
-  
+
   it("Success in defining the prime numbers of divisors", () => {
     const response = NumberServiceInstance.prime(dividerResponse);
 
