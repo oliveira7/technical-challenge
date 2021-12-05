746bd982007f225238d2ea6a06ee2c11a1d0fa11
Yuri Oliveira <deoliveirayuri7@yahoo.com>
2021-12-05 18:07:45 -0300

    feature/swagger-readme

---
+++ config/default.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+  "api": {
+    "port": process.env.PORT
+  }
+}
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const App = require("./App");
 const config = require("config");
 
 App.app.listen(config.get("api.port"), () => {
-  console.log("Api works!");
+  console.log(`Api works, PORT:${config.get("api.port")}`);
 });
 
 module.exports = App.app;
